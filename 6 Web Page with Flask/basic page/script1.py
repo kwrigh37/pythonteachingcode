e657f6de1db07eadc071cbd9e02b0d0d61d0c526
kwrigh37 <70081689+kwrigh37@users.noreply.github.com>
2020-12-10 22:03:35 -0500

    Completed Flask Demo Project

--- 6 Web Page with Flask/basic page/script1.py
+++ 6 Web Page with Flask/basic page/script1.py
@@ -16,5 +16,9 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route('/kara/')
+def kara():
+    return render_template("wright.html")
+
 if __name__=="__main__":
     app.run(debug=True)
